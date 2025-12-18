5bdace9ea5776cd24dc8d08eb8f748dfeda6a808
cassandrasantander <cassandrasantander03@gmail.com>
2025-12-18 03:51:07 -0800

    organization

--- finalproject/js/sound.js
+++ finalproject/js/sound.js
@@ -1,13 +1,12 @@
 
 
-
 let song = document.querySelector("#song");
 let playBtn = document.querySelector("#play-button");
 let pause = document.querySelector("#pause-button");
 
 playBtn.addEventListener('click', function () {
     song.play();
-    song.volume = .7;
+    song.volume = .5;
 })
 
 pause.addEventListener('click', function () {
@@ -19,3 +18,5 @@ song.onloadeddata = function(){
     playBtn.style.visibility = "visible";
 
 }
+
+console.log("the night sky");
