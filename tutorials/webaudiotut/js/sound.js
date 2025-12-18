7fbade62f6cd63a24e8eb27403d7b4031d53f6de
cassandrasantander <cassandrasantander03@gmail.com>
2025-12-17 20:33:14 -0800

    tut12

--- tutorials/webaudiotut/js/sound.js
+++ tutorials/webaudiotut/js/sound.js
@@ -1,13 +1,15 @@
 
+let loopBeat;
+let bassSynth;
 
 
-
-let song = document.querySelector("#song");
+let song = document.querySelector("#sfx");
 let playBtn = document.querySelector("#play-button");
 let pause = document.querySelector("#pause-button");
 
 playBtn.addEventListener('click', function () {
     song.play();
+    song.volume = .1;
 })
 
 pause.addEventListener('click', function () {
@@ -15,9 +17,25 @@ pause.addEventListener('click', function () {
 })
 
 
-
 song.onloadeddata = function(){
     playBtn.style.visibility = "visible";
 
 }
 
+
+
+function setup() {
+
+bassSynth = new Tone.MembraneSynth().toMaster();
+
+    loopBeat = new Tone.Loop(bsong, '4n');
+
+    Tone.Transport.bpm.value = 140;
+    Tone.Transport.start();
+    loopBeat.start(0);
+}
+
+function bsong(time) {
+    bassSynth.triggerAttackRelease('c1', '8n', time);
+    console.log(time )
+}
